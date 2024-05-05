85d2287cb1bb4f328c513f68e9a9ceec643fe98a
Esma Ekmekci <esoes@Esmas-MacBook-Air.local>
2024-05-05 15:35:35 +0300

    added informations from business

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,11 @@
-import Footage from "../components/About";
+import Footage from "../components/Footage";
 import Navbar from "../components/Navbar";
-
+import Information from "../components/Information";
 export default function About() {
   return (
     <>
       <Navbar />
+      <Information />
       <Footage />
     </>
   );
